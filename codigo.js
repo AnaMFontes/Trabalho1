f2d3099dde597602d82d73c4923411b519a5ab13
AnaMFontes <d2022031746@unifei.edu.br>
2022-05-25 15:42:20 -0300

    atualizações3

--- codigo.js
+++ codigo.js
@@ -1,11 +1,11 @@
 function calculaIdade() {
-    let AnoNasc, AnoAtual, resp;
+    let AnoNasc, AnoAtual, resp; //definindo variáveis
 
-    AnoAtual = document.getElementById("AnoAtual").value;
+    AnoAtual = document.getElementById("AnoAtual").value; //igualando a variável AnoAtual com a info. AnoAtual do html
 
-    AnoNasc = document.getElementById("AnoNasc").value;
+    AnoNasc = document.getElementById("AnoNasc").value; //igualando a variável AnoNasc com a info. AnoNasc do html
    
-    resp = parseInt(AnoAtual) - parseInt(AnoNasc);
+    resp = parseInt(AnoAtual) - parseInt(AnoNasc); //realizando a operação para calcular a idade
 
-    document.getElementById("resp").innerText = "Sua idade é: " + resp;
+    document.getElementById("resp").innerText = "Sua idade é: " + resp; //resposta + texto para aparecer no html
 }
